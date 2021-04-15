cef2f6919a17680b8434cc886404064b58714def
XAnubisX <73267314+XAnubisX@users.noreply.github.com>
2021-04-15 22:58:44 +0700

    Add files via upload

---
+++ 2zad.cpp
@@ -0,0 +1,174 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Product
+{
+	string name;
+	int quantity;
+	double price;
+	double total_cost = quantity * price;
+	void Out();
+};
+//Продукт
+void Product::Out() {
+	cout << endl << "Информация о первом товаре:" << endl;
+	cout << "\tИмя товара: " << name << endl;
+	cout << "\tКоличество: " << quantity << endl;
+	cout << "\tЦена: " << price << endl;
+	cout << "\tОбщая стоимость: " << total_cost << endl;
+}
+
+struct Queue
+{
+	struct Node { Product data; Node* next; };
+	Node *First = NULL;
+	int count = 0, rest = 0;
+	double money = 0;
+	bool Push(Product);
+	bool Pop(Product&);
+	void Info();
+};
+// Очередь добаление(Push)
+bool Queue::Push(Product data) {
+	if (!First) {
+		First = new Node;
+		First->next = NULL;
+		First->data = data;
+		count = 1;
+		rest = First->data.quantity;
+	}
+	else {
+		Node *temp;
+		temp = First;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new Node;
+		temp->next->data = data;
+		rest += temp->next->data.quantity;
+		temp->next->next = NULL;
+		count++;
+	}
+	return true;
+}
+// Очередь удаление(POP)
+bool Queue::Pop(Product& data) {
+	if (!First) return false;
+	Node* temp = First->next;
+	data = First->data;
+	delete First;
+	First = temp;
+	count--;
+	return true;
+}
+//Очередь инфо
+void Queue::Info() {
+	system("cls");
+	if (!First) cout << "---> Очередь пуста" << endl;
+	else {
+		cout << endl << "Информация о очереди: " << endl;
+		cout << "\tРазмер очереди = " << count << endl;
+		cout << "\tОстаток = " << rest << endl;
+		cout << "\tДеньги = " << money << endl;
+		First->data.Out();
+	}
+	system("pause");
+}
+
+void Menu();
+void Arriving(Queue&);
+void Sale(Queue&);
+//Меню
+void Menu() {
+	Queue q;
+	Product p;
+	int key;
+	do {
+		system("cls");
+		cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl;
+		cout << "1. Товары прибывают" << endl;
+		cout << "2. Продажа товаров" << endl;
+		cout << "3. Отчет" << endl;
+		cout << "0. Выход" << endl;
+		cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl;
+		cout << "Ввод: ";
+		cin >> key;
+
+		switch (key) {
+		case 1: Arriving(q); break;
+		case 2: Sale(q); break;
+		case 3: q.Info(); break;
+		case 0: {
+			while (q.count) {
+				q.Pop(p);
+			}
+		}break;
+		default:  system("cls");
+			cout << "Данные об ошибках, повторите попытку..." << endl;
+			system("pause");
+		}
+	} while (key);
+}
+//Добавление
+void Arriving(Queue &q) {
+	Product p;
+	string name;
+	int quantity;
+	double price;
+	cout << "Введите технические характеристики продукта(название, количество, цена): " << endl;
+	cin >> name >> quantity >> price;
+	while (quantity <= 0 or price < 0) {
+		cout << "Остаток и цена не могут быть меньше 0" << endl;
+		cout << "Количество: ";
+		cin >> quantity;
+		cout << "Цена: ";
+		cin >> price;
+	}
+	p = { name, quantity, price };
+	q.Push(p);
+}
+
+//Продажа
+void Sale(Queue &q) {
+	Product product;
+	int count;
+	double price;
+	product = q.First->data;
+	cout << "Введите количество и стоимость единиц товара, который вы хотите приобрести: ";
+	cin >> count >> price;
+	while (count <= 0 || count > q.rest || price <= 0 || price <= product.price) {
+		cout << "---> Не корректно введены данный <--- " << endl;
+		cout << "Количество товара: ";
+		cin >> count;
+		cout << "Стоимость: ";
+		cin >> price;
+	}
+	while (count) {
+		if (count <= product.quantity) {
+			q.money += count * (price - product.price);
+			product.total_cost -= count * product.price;
+			product.quantity -= count;
+			q.rest -= count;
+			count = 0;
+			q.First->data.quantity = product.quantity;
+			q.First->data.total_cost = product.total_cost;
+		}
+		else {
+			q.money += product.quantity * (price - product.price);
+			count -= product.quantity;
+			q.rest -= count;
+			product.quantity = 0;
+		}
+		if (product.quantity == 0) {
+			q.Pop(product);
+			product = q.First->data;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Menu();
+	return 0;
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,196 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+struct Ball {
+	string color;
+	double price;
+	string proz;
+	double size;
+	void Out();
+};
+
+void Ball::Out() {
+	cout << "\tЦвет: " << color << endl;
+	cout << "\tЦена: " << price << endl;
+	cout << "\tПроизводитель: " << proz << endl;
+	cout << "\tРазмер:" << size << endl;
+}
+
+struct Stack
+{
+	struct Node
+	{
+		Ball data; Node* prev;
+	};
+	Node *top = NULL;
+	int count;
+	bool Push(Ball);
+	bool Pop(Ball&);
+	void Info();
+};
+
+//вставка
+bool Stack::Push(Ball data) {
+	if (!top) {
+		top = new Node;
+		top->prev = NULL;
+		count = 1;
+	}
+	else {
+		Node *temp;
+		temp = new Node;
+		temp->prev = top;
+		top = temp;
+		count++;
+	}
+	top->data = data;
+	return true;
+}
+
+//удаление
+bool Stack::Pop(Ball& data) {
+	if (!top) return false;
+	Node* temp = top->prev;
+	data = top->data;
+	delete top;
+	top = temp;
+	count--;
+	return true;
+}
+
+//Стек инфо
+void Stack::Info() {
+	if (!top)
+		cout << "---> Стек пустой" << endl;
+	else {
+		cout << "Информация о стеке: " << endl;
+		cout << "\tРазмер стека = " << count << endl;
+		top->data.Out();
+		cout << endl;
+	}
+}
+
+void Menu();
+void readFile(Stack&);
+void userPush(Stack&);
+void getOut(Stack&);
+void Clear(Ball&, Stack&);
+
+//Меню
+void Menu() {
+	Stack s;
+	Ball p;
+	readFile(s);
+	int key;
+	do {
+		system("cls");
+		cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl;
+		cout << "1. Добавить продукт в корзину" << endl;
+		cout << "2. Достать продукт из корзины" << endl;
+		cout << "3. Очистить корзину" << endl;
+		cout << "0. Выход" << endl;
+		cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl;
+		s.Info();
+		cout << "Ввод: ";
+		cin >> key;
+
+		switch (key) {
+		case 1: userPush(s); break;
+		case 2: getOut(s); break;
+		case 3: Clear(p, s); break;
+		case 0: break;
+		default:  system("cls");
+			cout << "Данные об ошибках, повторите попытку..." << endl;
+			system("pause");
+		}
+	} while (key);
+}
+
+//Файл
+void readFile(Stack &stack) {
+	Ball ball;
+	ifstream data;
+	data.open("data.txt", ios::in);
+	if (!data) {
+		cout << "---> Ошибка открытия файла <---" << endl;
+		return;
+	}
+	else {
+		while (data >> ball.color >> ball.price >> ball.proz >> ball.size) {
+			stack.Push(ball);
+		}
+		data.close();
+	}
+} 
+
+//Ввод
+void userPush(Stack& s) {
+	string color, proz;
+	double price, size;
+	Ball ball;
+	cout << "Введите товар (цвет, цена, производитель, размер): ";
+	cin >> color >> price >> proz >> size;
+	while (price < 0 or size <= 0) {
+		cout << "---> Цена или размер не могут быть меньше 0 <---" << endl;
+		cout << "Цена: ";
+		cin >> price;
+		cout << "Размер: ";
+		cin >> size;
+	}
+	ball = { color, price, proz, size };
+	s.Push(ball);
+}
+
+//Достать продукт
+void getOut(Stack& s) {
+	Stack temp;
+	Ball top;
+	string color, proz;
+	double price, size;
+	bool flag = true;
+	if (s.count == 0) {
+		cout << "No products in the cart" << endl;
+		system("pause");
+		return;
+	}
+	cout << "Введите параметры товара(цвет, цена, производитель, размер): ";
+	cin >> color >> price >> proz >> size;
+	while (flag) {
+		top = s.top->data;
+		if ((color == top.color) && (price == top.price) && (proz == top.proz) && (size == top.size)) {
+			s.Pop(top);
+			flag = 0;
+			break;
+		}
+		else if (s.count > 0) {
+			s.Pop(top);
+			temp.Push(top);
+		}
+		if (s.count == 0) break;
+	}
+	while (temp.count > 0) {
+		top = temp.top->data;
+		s.Push(top);
+		temp.Pop(top);
+	}
+	if (flag) cout << "В корзине нет товара" << endl;
+	else cout << "Продукт был найден" << endl;
+	system("pause");
+}
+
+//Очистить стек
+void Clear(Ball& ball, Stack &s) {
+	while (s.count) {
+		s.Pop(ball);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Menu();
+	return 0;
+}
